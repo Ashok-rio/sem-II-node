9d1a652b8d8b3a910aabe626f20b453e3df15a5b
Ranjith <ranjithkumarranjith1999@gmail.com>
2020-06-28 18:18:55 +0530

    Added new packages, api,user.modal,user.controller,modify (app.js ,www,auth.services)

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,21 @@
 // API
+const express = require('express')
+const router = express.Router()
+
+const UserController = require('../controllers/User.controller')
+const passport = require('passport')
+const path = require('path')
+
+const jwtAuth = require('../middleware/passport')
+
+const authUser = jwtAuth(passport).authenticate('jwt', {session: false})
+
+//user API's
+
+//user registration
+
+router.post('/user/register', UserController.userRegsiter)
+router.post('/user/login', UserController.login)
+
+
+module.exports = router
